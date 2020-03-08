c6f343346e63b78304c17b1ef8fda4a124858616
ZephyrRaine <zephyr.raine@gmail.com>
2020-03-08 10:17:06 +0100

    Clouds & Plants events

--- Assets/Sources/Power/PlayerScore.cs
+++ Assets/Sources/Power/PlayerScore.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class PlayerScore : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class PlayerScore : MonoBehaviour
     public Text humanText = null;
     public Text sunText = null;
     public Text plantText = null;
+
+    public Action<bool> cloudsChangeEvent;
+    public Action<bool> plantsChangeEvent;
 
 
     public Image lightImage = null;
@@ -27,11 +31,11 @@ public class PlayerScore : MonoBehaviour
     private int _score = 0;
 
     public int waterLevel { get { return _waterLevel; } set { _waterLevel = value; waterText.text = "water " + _waterLevel; } }
-    public int animalCount { get { return _animalCount; } set {  _animalCount = value; animalText.text = "animal " + _animalCount; } }
+    public int animalCount { get { return _animalCount; } set { _animalCount = value; animalText.text = "animal " + _animalCount; } }
     public int totalAnimalsCount { get { return _totalAnimalsCount; } set {  _totalAnimalsCount = value; totalAnimals.text = "TOTAL " + _totalAnimalsCount; } }
-    public int cloudCount { get { return _cloudCount; } set { _cloudCount = value; cloudText.text = "cloud " + _cloudCount; } }
+    public int cloudCount { get { return _cloudCount; } set { bool add = value > _cloudCount; cloudsChangeEvent.Invoke(add); _cloudCount = value; cloudText.text = "cloud " + _cloudCount; } }
     public int humanCount { get { return _humanCount; } set { _humanCount = value; humanText.text = "human " + _humanCount; } }
-    public int plantCount { get { return _plantCount; } set { _plantCount = value; plantText.text = "plant " + _plantCount; } }
+    public int plantCount { get { return _plantCount; } set { bool add = value > _plantCount; plantsChangeEvent.Invoke(add); _plantCount = value; plantText.text = "plant " + _plantCount; } }
     public bool sun { get { return _sun; } set { _sun = value; sunText.text = _sun?"SUN":"MOON"; } }
     public bool lightOff { get { return _lightOff; } set { _lightOff = value; lightImage.enabled = value; }}
 
